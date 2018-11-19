9cc0a89592e0737065bf22b99064eea64d55cf84
gbeagle <43616450+gbeagle@users.noreply.github.com>
2018-11-19 10:29:39 -0600

    Update day6.py

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,26 +1,32 @@
 import re
 from numpy import count_nonzero
 
-def follow_directions(instruction, lights):
+def follow_directions(instruction, lights1, lights2):
     (lightFlip, x1, y1, x2, y2) = instruction
     for x in range(int(x1), (int(x2)+1)):
         for y in range(int(y1), (int(y2)+1)):
             if lightFlip == 'turn on':
-                lights[x][y] = 1
+                lights1[x][y] = 1
+                lights2[x][y] += 1
             elif lightFlip == 'turn off':
-                lights[x][y] = 0
+                lights1[x][y] = 0
+                if lights2[x][y] > 0: lights2[x][y] -= 1
             elif lightFlip == 'toggle':
-                if lights[x][y] == 1: lights[x][y] = 0
-                elif lights[x][y] == 0: lights[x][y] = 1
-    return lights
+                if lights1[x][y] == 1: lights1[x][y] = 0
+                elif lights1[x][y] == 0: lights1[x][y] = 1
+                lights2[x][y] += 2
+    return lights1, lights2
 
 
 pattern = re.compile("(turn on|toggle|turn off)\s([\w]+),([\w]+)\sthrough\s([\w]+),([\w]+)")
 testInfo = ['turn on 0,0 through 999,999', 'toggle 0,0 through 999,0', 'turn off 499,499 through 500,500']
-lights = [[0 for x in range(1000)] for y in range(1000)]
+lights1 = [[0 for x in range(1000)] for y in range(1000)]
+lights2 = [[0 for x in range(1000)] for y in range(1000)]
 
-for line in testInfo:
-    direction = pattern.search(line).groups()
-    lights = follow_directions(direction, lights)
+with open('day6inputs.txt') as f:
+    for line in f:
+        direction = pattern.search(line).groups()
+        (lights1, lights2) = follow_directions(direction, lights1, lights2)
 
-print count_nonzero(lights)
+print count_nonzero(lights1)
+print sum(sum(x) for x in lights2)
